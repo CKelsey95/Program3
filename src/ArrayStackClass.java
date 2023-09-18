7fee903de51f9caad844085bd147dea379d30338
CKelsey95 <colton.e.kelsey@gmail.com>
2023-09-18 16:43:34 -0600

    initial setup

---
+++ src/ArrayStackClass.java
@@ -0,0 +1,28 @@
+/* Colton Kelsey
+   CS 2050 Program 3
+*/
+
+public class ArrayStackClass {
+
+    push(){
+
+    }
+
+    pop(){
+
+    }
+
+    empty(){
+
+    }
+
+    size(){
+
+    }
+
+    setArrayStack(){
+        int n;
+
+    }
+
+}
---
+++ src/Program3.java
@@ -0,0 +1,15 @@
+/* Colton Kelsey
+   CS 2050 Program 3
+*/
+
+public class Program3 {
+    public static void main(String[] args) {
+    String Input = "4 + 6 * 3.1";
+
+    public static void InfixToPostfix (String Input){
+
+        }
+
+
+    }
+}
