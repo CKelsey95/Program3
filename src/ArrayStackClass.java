ac900f0ebeaeca6aca958e4ed627da8eebf9c01c
CKelsey95 <colton.e.kelsey@gmail.com>
2023-09-18 22:09:29 -0600

    constructor configured.

--- src/ArrayStackClass.java
+++ src/ArrayStackClass.java
@@ -1,14 +1,32 @@
 /* Colton Kelsey
    CS 2050 Program 3
 */
+import java.util.EmptyStackException;
 
-public class ArrayStackClass {
+public class ArrayStackClass<T> {
+    private T[] items;
+    int top;    // current amount stored in the stack
+    int maxSize;  // max size of the stack
 
-    push(){
-
+    public ArrayStackClass(int N){
+        maxSize = N;
+        items = (T[]) new Object[maxSize];
+        top = 0;
+    }
+    public void push(T element){
+    if (top== maxSize){
+        throw new StackOverflowError();
+    }
+    else {
+        items[top++] = (T) element;
+    }
     }
 
     pop(){
+
+    }
+
+    peek(){
 
     }
 
@@ -17,11 +35,6 @@ public class ArrayStackClass {
     }
 
     size(){
-
-    }
-
-    setArrayStack(){
-        int n;
 
     }
 
--- src/Program3.java
+++ src/Program3.java
@@ -1,7 +1,6 @@
 /* Colton Kelsey
    CS 2050 Program 3
 */
-
 public class Program3 {
     public static void main(String[] args) {
     String Input = "4 + 6 * 3.1";
